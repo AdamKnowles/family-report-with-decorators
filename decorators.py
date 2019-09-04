ec7c66b10a08fe2ed60ddcb284d12f2a866d2155
Adam Knowles <acknowles524@gmail.com>
2019-09-04 12:05:59 -0500

    now its actually done

--- decorators.py
+++ decorators.py
@@ -1,47 +1,44 @@
 def kids(func):
     def kids_say():
-        func()
-        print("by the kids")
+        print(f'{func()} by the kids')
 
     return kids_say
 
 def dad(func):
-    def dad_says():
-        func()
-        print("by dad")
+    def dad_says(): 
+        print(f'{func()} by dad')
 
     
     return dad_says
 
 def mom(func):
     def mom_says():
-        func()
-        print("by mom")
+        print(f'{func()} by mom')
 
     return mom_says
     
 
 @mom
 def laundry():
-    print("The dirty laundry was cleaned")
+    return "The dirty laundry was cleaned"
 
 laundry()
 
 @kids
 def garbage():
-    print ("The garbage was taken out")
+    return "The garbage was taken out"
 
 garbage()
 
 @kids
 def dust():
-    print ("The house was dusted")
+    return "The house was dusted"
 
 dust()
 
 @dad
 def groom():
-    print ("The dog was brushed")
+    return "The dog was brushed"
 
 groom()
 
